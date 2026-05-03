61689ccda1f55bb2e76a63fbfc12a2ef05904bdd
admin <kattubadi426@gmail.com>
2026-05-03 11:49:06 +0530

    latest

--- src/utility/helpers.py
+++ src/utility/helpers.py
@@ -1,6 +1,7 @@
 import os
-from pyspark.sql.types import StructType
+from pyspark.sql.types import *
 import json
+from pyspark.sql.functions import col, explode_outer
 
 def read_sql():
     pass
@@ -15,3 +16,30 @@ def read_schema(dir_path):
         print("schema is", schema)
         print(("==" * 100))
     return schema
+
+def flatten(df):
+    # compute Complex Fields (Lists and Structs) in Schema
+    complex_fields = dict([(field.name, field.dataType)
+                           for field in df.schema.fields
+                           if type(field.dataType) == ArrayType or type(field.dataType) == StructType])
+    while len(complex_fields) != 0:
+        col_name = list(complex_fields.keys())[0]
+        print("Processing :" + col_name + " Type : " + str(type(complex_fields[col_name])))
+
+        # if StructType then convert all sub element to columns.
+        # i.e. flatten structs
+        if type(complex_fields[col_name]) == StructType:
+            expanded = [col(col_name + '.' + k).alias(col_name + '_' + k) for k in
+                        [n.name for n in complex_fields[col_name]]]
+            df = df.select("*", *expanded).drop(col_name)
+
+        # if ArrayType then add the Array Elements as Rows using the explode function
+        # i.e. explode Arrays
+        elif type(complex_fields[col_name]) == ArrayType:
+            df = df.withColumn(col_name, explode_outer(col_name))
+
+        # recompute remaining Complex Fields in Schema
+        complex_fields = dict([(field.name, field.dataType)
+                               for field in df.schema.fields
+                               if type(field.dataType) == ArrayType or type(field.dataType) == StructType])
+    return df
--- src/utility/read_file_lib.py
+++ src/utility/read_file_lib.py
@@ -1,4 +1,4 @@
-from src.utility.helpers import read_schema
+from src.utility.helpers import read_schema,flatten
 
 def read_file(spark, file_type, file_path,file_config,dir_path):
     file_type = file_type.lower()
@@ -14,6 +14,26 @@ def read_file(spark, file_type, file_path,file_config,dir_path):
                                  header=file_config['header'],
                                  sep=file_config['sep'],
                                  inferSchema=file_config['inferSchema']))
+    elif file_type == 'json':
+        if file_config['multiline']:
+            df = spark.read.json(path=file_path,multiLine=file_config['multiline']==True )
+            df.show()
+            df = flatten(df)
+        else:
+            df = spark.read.json(path=file_path, multiLine=file_config['multiline'] == False)
+            df = flatten(df)
+    elif file_type == 'parquet':
+        df = spark.read.parquet(file_path)
+    elif file_type == 'avro':
+        df = spark.read.format('avro').load(file_path)
+    elif file_type == 'xml':
+        df = spark.read.xml('avro').load(file_path)
+    elif file_type == 'txt':
+        df = (spark.read.csv(file_path,
+                             header=file_config['header'],
+                             sep=file_config['sep'],
+                             inferSchema=file_config['inferSchema']))
+
     return df
 
 #csv, json, parquet, avro, txt, xml
